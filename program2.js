3f83ab755afa2ae60d8f9e7a033db770552f7b3f
Aditya Patel <99285566+AdityaPatel9506@users.noreply.github.com>
2024-01-05 13:41:56 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -3,21 +3,27 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    let map = new Map([['I', 1], ['V', 5], ['X', 10], ['L', 50], ['C', 100], ['D', 500], ['M', 1000]]);
-    let sum = 0;
-    let prev = 0;
-    
-    for (let i = s.length - 1; i >= 0; i--) {
-        let curr = map.get(s[i]);
-        if (prev && curr >= prev) {
-            sum += curr;
-        } else {
-            sum -= curr;
-        }
-        prev = curr;
+    const legend = {
+    I:1,
+    V:5,
+    X:10,
+    L:50,
+    C:100,
+    D:500,
+    M:1000
+  };
+  let total = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (legend[s[i]] < legend[s[i+1]]) {
+      total += legend[s[i+1]] - legend[s[i]];
+      i++;
+    } else {
+      total += legend[s[i]];
     }
-    
-    return sum;
+  }
+
+  return total;
 };
 
 
